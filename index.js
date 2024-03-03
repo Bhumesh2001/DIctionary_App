7ddb99a5b6c0ea22a05ec0f7e6d6bc29c0dbac5c
bhumesh2001 <bhumesh21@navgurukul.org>
2024-03-03 13:54:39 +0530

    added new fetures

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const getWordDefinition = async function () {
-    let word = 'hello';
+const loader = document.getElementById('search-loader');
+const card_tag = document.querySelector('.card');
+const ulTag = document.querySelector('.card-content');
+
+document.getElementById('Search-btn').addEventListener('click', () => {
+    const searchValue = document.querySelector('input').value.trim();
+    if (searchValue !== '') {
+        loader.style.display = 'block';
+        ulTag.innerHTML = '';
+        card_tag.style.display = 'none';
+
+        setTimeout(() => {
+            getWordDefinition(searchValue);
+        }, 3000);
+    };
+});
+
+const getWordDefinition = async function (word) {
     const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
     const info = await data.json();
-    console.log(info[0]);
+
+    loader.style.display = 'none';
+
+    if (Array.isArray(info)) {
+        info[0].meanings.forEach(element => {
+            if (element.partOfSpeech !== 'conjunction') {
+                element.definitions.forEach(def => {
+                    const liTag = document.createElement('li');
+                    const p_tag = document.createElement('p');
+                    p_tag.textContent = def.definition;
+                    liTag.appendChild(p_tag);
+                    ulTag.appendChild(liTag);
+                });
+            };
+        });
+        card_tag.appendChild(ulTag);
+        card_tag.style.display = 'block';
+    } else {
+        console.log(info.title);
+    };
 };
-getWordDefinition();
